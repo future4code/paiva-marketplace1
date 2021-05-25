df27b7275748329c7ab23cec5461aab1f01c5b81
Adrian Américo <1fnadrian1@gmail.com>
2021-05-25 19:22:53 -0300

    correcao de erros no merge

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { AppContainer } from "./components/AppContainer";
 import { Header } from "./components/Header/Header";
-// import { Body } from "./components/Body/Body";
-import GlobalStyle from './Styles/Global';
-import Body from './components/LandingPage/Body'
-// import { Trabalho } from "./components/Trabalho/Trabalho";
-// import { Talento } from "./components/Talento/Talento";
-// import Vitrine from './components/Vitrine/Vitrine'
-//import { Footer } from "./components/Footer/Footer";
-
+import { Body } from "./components/Body/Body";
+import { Trabalho } from "./components/Trabalho/Trabalho";
+import { Talento } from "./components/Talento/Talento";
+import Vitrine from './components/Vitrine/Vitrine'
+import Banner from './components/Banner/Banner'
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Body />
-			{/* <Trabalho /> */}
-			{/* <Talento /> */}
+			<Banner />
+			{/* <Body/> */}
+			<Vitrine />
+			{/* <Trabalho/>
+			<Talento/> */}
 			<AppContainer />
-			{/* <Vitrine /> */
-			<GlobalStyle />
-
-
 		</div>
 	);
 }
---
+++ src/components/Vitrine/Images.js
@@ -0,0 +1,8 @@
+import Pinho from './img/Pinho.jpg'
+import Mtv from './img/MTV.jpg'
+
+
+export const images = [
+    { title: "Pinho", img: Pinho },
+    { title: "Mtv",  img: Mtv },
+  ];
